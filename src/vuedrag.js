b894cc8a01e05845251aa27de5fd8a28de60dd8d
remu1519 <bf3luxo71outi0@gmail.com>
2020-03-23 18:37:30 +0900

    feat: とりあえずサンプルまで作成

---
+++ src/vuedrag.js
@@ -0,0 +1,23 @@
+import VueDrag from './components/VueDrag';
+
+export const install = Vue => {
+    if (install.installed) return;
+    install.installed = true;
+    Vue.component('vue-drag', VueDrag);
+}
+
+const plugin = {
+    install
+}
+
+const GlobalVue = (() => {
+    if (typeof window !== 'undefined') return window.Vue;
+    if (typeof global !== 'undefined') return global.Vue;
+    return null;
+})();
+
+if (GlobalVue) {
+    GlobalVue.use(plugin);
+}
+
+export default VueDrag;
